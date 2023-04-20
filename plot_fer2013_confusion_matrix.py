aaafd19fd20e60ebf2533bcceaa22d4eaaacd935
Vibhu Gupta <vibhu7gupta@gmail.com>
2023-04-20 19:04:21 +0530

    Update plot_fer2013_confusion_matrix.py

--- plot_fer2013_confusion_matrix.py
+++ plot_fer2013_confusion_matrix.py
@@ -79,7 +79,7 @@ elif opt.model  == 'Resnet18':
     net = ResNet18()
 
 path = os.path.join(opt.dataset + '_' + opt.model)
-checkpoint = torch.load(os.path.join(path, opt.split + '_model.t7'))
+checkpoint = torch.load("/content/PrivateTest_model.t7"))
 
 net.load_state_dict(checkpoint['net'])
 net.cuda()
